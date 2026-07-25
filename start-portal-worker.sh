d9abe52bb8c76a03b927ef8d722bec71b43a0a58
findandstudy <findandstudy@gmail.com>
2026-07-25 03:08:31 +0300

    chore(altinbas): enable verified UI completion

--- start-portal-worker.sh
+++ start-portal-worker.sh
@@ -2,5 +2,7 @@
 set -a
 . /var/www/apply.findandstudy.com/.env
 set +a
+export ALTINBAS_UI_COMPLETE="${ALTINBAS_UI_COMPLETE:-1}"
+export ALTINBAS_CAPTURE="${ALTINBAS_CAPTURE:-0}"
 cd /var/www/apply.findandstudy.com/artifacts/portal-automation-worker
 exec /usr/bin/pnpm run start
